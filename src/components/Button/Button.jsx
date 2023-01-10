83c46d6a1c8baf0d6c04a33763d173a66022f773
MrMishka02 <m.gogia@hotmail.com>
2023-01-11 00:01:39 +0400

    added style components

--- src/components/Button/Button.jsx
+++ src/components/Button/Button.jsx
@@ -1,6 +1,32 @@
+import { hover } from "@testing-library/user-event/dist/hover";
+import styled from "styled-components";
+
+const StyledButton = styled.button`
+  width: 387px;
+  height: 60px;
+  background: #62A1EB;
+  border-radius: 8px;
+  margin-bottom: 26px;
+  font-family: 'Helvetica Neue';
+  font-style: normal;
+  font-weight: 500;
+  font-size: 20.5px;
+  line-height: 24px;
+  color: #FFFFFF;
+  border: none;
+  :hover {
+    cursor: pointer;
+    background-color: #317AD0;
+  }
+  :active{
+    cursor: pointer;
+    background-color: #1A5DAB;
+  }
+`
+
 function Button({text}) {
   return (
-    <button className='landBtn'>{text}</button>
+    <StyledButton>{text}</StyledButton>
   );
 }
 
--- src/components/Button/CircleButton.jsx
+++ src/components/Button/CircleButton.jsx
@@ -1,6 +1,24 @@
+import styled from "styled-components";
+
+const StyledCircleBtn = styled.button`
+  position: absolute;
+  left: 70px;
+  top: 53px;
+  width: 54px;
+  height: 54px;
+  background: #D9D9D9;
+  color: #2E2E2E;
+  font-size: 22px;
+  border-radius: 50%;
+  border: none;
+  :hover{
+    cursor: pointer;
+  }
+`
+
 function CircleButton() {
     return (
-      <button className='circleBtn'>&#x276E;</button>
+      <StyledCircleBtn>&#x276E;</StyledCircleBtn>
     );
   }
 
