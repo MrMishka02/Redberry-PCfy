8596a403ebb080a315df5724417c63d1a7776766
MrMishka02 <m.gogia@hotmail.com>
2023-01-16 22:35:34 +0400

    fix: mobile version

--- src/pages/EntriesList/EntriesList.jsx
+++ src/pages/EntriesList/EntriesList.jsx
@@ -3,22 +3,29 @@ import PCList from "../../components/PCList/PCList";
 
 function EntriesList() {
   return (
-    <div className="flex w-full flex-col items-center bg-[#FFFFFF]">
+    <div className="flex w-full h-full flex-col items-center bg-[#FFFFFF]">
       <CircleButton path={"/"} />
-      <div className="h- flex w-full justify-center">
-        <p className="mt-16 text-[2.125rem] font-bold
-        sm:text-base sm:mt-8">ᲩᲐᲜᲐᲬᲔᲠᲔᲑᲘᲡ ᲡᲘᲐ</p>
+      <div className="flex w-full justify-center">
+        <p
+          className="mt-16 text-[2.125rem] font-bold
+        sm:mt-8 sm:text-base"
+        >
+          ᲩᲐᲜᲐᲬᲔᲠᲔᲑᲘᲡ ᲡᲘᲐ
+        </p>
       </div>
-      <div className="mt-20 mb-20 grid h-[61.5rem] w-[73.5rem] grid-cols-2 gap-14
-      sm:flex sm:flex-col sm:w-[24.375rem] sm:items-center sm:gap-4">
-          <PCList />
-          <PCList />
-          <PCList />
-          <PCList />
-          <PCList />
-          <PCList />
-          <PCList />
-          <PCList />
+      <div
+        className="mt-20 mb-20 grid h-full w-[73.5rem] grid-cols-2 gap-14
+        xl:flex xl:flex-col xl:items-center xl:w-[30rem]
+      sm:flex sm:w-[24.375rem] sm:flex-col sm:items-center sm:gap-4"
+      >
+        <PCList />
+        <PCList />
+        <PCList />
+        <PCList />
+        <PCList />
+        <PCList />
+        <PCList />
+        <PCList />
       </div>
     </div>
   );
