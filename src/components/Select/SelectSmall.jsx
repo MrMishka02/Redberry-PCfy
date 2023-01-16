ac0c412a897ce62baeb74df44576caf8c2395f1e
MrMishka02 <m.gogia@hotmail.com>
2023-01-16 15:54:07 +0400

    style: correct element margin which cause overflow

--- src/components/Select/SelectSmall.jsx
+++ src/components/Select/SelectSmall.jsx
@@ -4,7 +4,7 @@ const SelectSmall = ({ name, value, choose }) => {
       name={name}
       value={value}
       className="mt-[0.45rem] mr-6 h-[3.75rem] w-full rounded-[0.5rem] border-none
-      bg-[#EBEBEB] pl-2 text-[1.125rem] leading-[1.31rem] text-[#000000]"
+      bg-[#EBEBEB] pl-2 text-[1.125rem] leading-[1.31rem] text-[#000000] sm:mr-0"
     >
       <option value="">{choose}</option>
     </select>
