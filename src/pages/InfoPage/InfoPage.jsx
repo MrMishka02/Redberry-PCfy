810b0f479aa9d1cf44e4f966a6884fc9d99ee87a
MrMishka02 <m.gogia@hotmail.com>
2023-01-17 10:25:26 +0400

    style: fixed responsive

--- src/pages/InfoPage/InfoPage.jsx
+++ src/pages/InfoPage/InfoPage.jsx
@@ -10,7 +10,7 @@ const InfoPage = () => {
     <div className="flex h-full w-full flex-col items-center bg-[#FFFFFF]">
       <CircleButton path={"/entries-list"} />
       <InfoHeadLabel text={"ᲚᲔᲞᲢᲝᲞᲘᲡ ᲘᲜᲤᲝ"} />
-      <div className="mt-16 flex w-[60%] sm:mt-6 sm:w-full sm:flex-col">
+      <div className="mt-16 flex w-[70%] sm:mt-6 sm:w-full sm:flex-col items-center">
         <div className="w-[50%] sm:flex sm:w-full sm:justify-center">
           <img
             src={laptop}
@@ -19,7 +19,7 @@ const InfoPage = () => {
           />
         </div>
         <div className="flex w-[50%] sm:mt-4 sm:w-full">
-          <div className="ml-14 mt-8 w-[30%] sm:ml-4 sm:mt-0 sm:w-[50%]">
+          <div className="ml-14 mt-8 w-[30%] sm:ml-10 sm:mt-0 sm:w-[45%]">
             <InfoLabel text={"სახელი: "} />
             <InfoLabel text={"თიმი: "} />
             <InfoLabel text={"პოზიცია: "} />
@@ -35,26 +35,26 @@ const InfoPage = () => {
           </div>
         </div>
       </div>
-      <div className="mt-[5.3rem] w-[60%] sm:mt-[2.4rem] sm:w-[95%]">
+      <div className="mt-[5.3rem] w-[70%] sm:mt-[2.4rem] sm:w-[95%]">
         <Line />
       </div>
-      <div className="mt-11 flex w-[60%] sm:mt-5 sm:w-full sm:flex-col sm:overflow-x-hidden">
-        <div className="flex w-[50%] sm:ml-4 sm:w-[100%] ">
-          <div className="w-[50%] sm:w-[48%]">
+      <div className="mt-11 flex w-[70%] sm:mt-5 sm:w-full sm:flex-col sm:overflow-x-hidden">
+        <div className="flex w-[50%] sm:ml-10 sm:w-[100%] ">
+          <div className="w-[55%] sm:w-[44%]">
             <InfoLabel text={"ლეპტოპის სახელი: "} />
             <InfoLabel text={"ლეპტოპის ბრენდი: "} />
             <InfoLabel text={"RAM: "} />
             <InfoLabel text={"მეხსიერების ტიპი: "} />
           </div>
-          <div className="w-[50%] sm:w-[46%]">
+          <div className="w-[45%] sm:w-[46%]">
             <InfoText text={"Razor Bla Bla"} />
             <InfoText text={"HP"} />
             <InfoText text={"16"} />
             <InfoText text={"SSD"} />
           </div>
         </div>
-        <div className="flex w-[50%] pl-14 sm:ml-4 sm:mt-2 sm:w-full sm:pl-0">
-          <div className="w-[44%] sm:w-[48%]">
+        <div className="flex w-[50%] pl-14 sm:ml-10 sm:mt-2 sm:w-full sm:pl-0">
+          <div className="w-[45%] sm:w-[44%]">
             <InfoLabel text={"CPU: "} />
             <InfoLabel text={"CPU-ს ბირთვი: "} />
             <InfoLabel text={"CPU-ს ნაკადი: "} />
@@ -66,12 +66,12 @@ const InfoPage = () => {
           </div>
         </div>
       </div>
-      <div className="mt-[3.8rem] w-[60%] sm:mt-[2.8rem] sm:w-[95%]">
+      <div className="mt-[3.8rem] w-[70%] sm:mt-[2.8rem] sm:w-[95%]">
         <Line />
       </div>
-      <div className="mt-11 flex w-[60%] sm:mt-4 sm:w-full sm:flex-col">
-        <div className="flex w-[50%] sm:ml-4 sm:flex sm:w-full">
-          <div className="w-[50%] sm:w-[48%]">
+      <div className="mt-11 flex w-[70%] sm:mt-4 sm:w-full sm:flex-col sm:overflow-x-hidden">
+        <div className="flex w-[50%] sm:ml-10 sm:flex sm:w-full">
+          <div className="w-[55%] sm:w-[44%]">
             <div className="sm:hidden">
               <InfoLabel text={"ლეპტოპის მდგომარეობა: "} />
             </div>
@@ -80,13 +80,13 @@ const InfoPage = () => {
             </div>
             <InfoLabel text={"ლეპტოპის ფასი: "} />
           </div>
-          <div className="w-[50%] sm:w-[50%]">
+          <div className="w-[45%] sm:w-[50%]">
             <InfoText text={"მეორადი"} />
             <InfoText text={"1500 ლ"} />
           </div>
         </div>
         <div className="flex w-[50%] sm:w-full">
-          <div className="w-[45%] pl-14 sm:ml-4 sm:w-[50%] sm:pl-0">
+          <div className="w-[45%] pl-14 sm:ml-10 sm:w-[48%] sm:pl-0">
             <InfoLabel text={"შეძენის რიცხვი: "} />
           </div>
           <div className="w-[50%] pl-7 sm:w-[50%] sm:pl-0">
