4f9ee7312ebc80f52cf6bbdfb72f5682aa678fc1
MrMishka02 <m.gogia@hotmail.com>
2023-01-25 17:57:53 +0400

    chore: added font folder to assets folder

--- src/index.js
+++ src/index.js
@@ -2,7 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import './fonts/HelveticaNeue.ttc'
+import './assets/fonts/HelveticaNeue.ttc'
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
